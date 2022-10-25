e66da8f0b4b0e1067a440268040b0dc93eacbf7d
JayZhouCom <113875840+JayZhouCom@users.noreply.github.com>
2022-10-25 11:14:05 +0800

    done

---
+++ src/declarations.ts
@@ -0,0 +1,13 @@
+interface OKXWallet {
+  send: unknown
+  enable: () => Promise<string[]>
+  on?: (method: string, listener: (...args: any[]) => void) => void
+  removeListener?: (method: string, listener: (...args: any[]) => void) => void
+}
+
+declare interface Window {
+  okxwallet?: OKXWallet;
+  isOkxWallet?: boolean;
+}
+
+declare const __DEV__: boolean
